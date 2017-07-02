89912b656a659c90d39932839564edfab0b86a0c
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-02 12:51:19 +0000

    Fix code style

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,20 +1,16 @@
 #!/bin/bash -e
 
-bootstrap() {
-  local ARCH="$(dpkg --print-architecture)"
+if [ ! -d "$ROOTFS_DIR" ]; then
+  ARCH="$(dpkg --print-architecture)"
 
   if [ "$ARCH" != 'armhf' ]; then
-    local BOOTSTRAP_CMD='qemu-debootstrap'
+    BOOTSTRAP_CMD='qemu-debootstrap'
   else
-    local BOOTSTRAP_CMD='debootstrap'
+    BOOTSTRAP_CMD='debootstrap'
   fi
 
   capsh --drop=cap_setfcap -- -c "$BOOTSTRAP_CMD --components=main,contrib,non-free \
     --arch armhf \
-    --keyring "$STAGE_DIR/files/raspberrypi.gpg" \
-    $1 $2 $3" || rmdir "$2/debootstrap"
-}
-
-if [ ! -d "$ROOTFS_DIR" ]; then
-  bootstrap jessie "$ROOTFS_DIR" http://mirrordirector.raspbian.org/raspbian/
+    --keyring $STAGE_DIR/files/raspberrypi.gpg \
+    jessie $ROOTFS_DIR http://mirrordirector.raspbian.org/raspbian/" || rmdir "$ROOTFS_DIR/debootstrap"
 fi
