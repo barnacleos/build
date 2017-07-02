41201502918d97554b4666bfdf7d533e4f428bc8
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-02 18:45:50 +0000

    Fix code style

--- stage0/17-set-partuuid/00-run.sh
+++ stage0/17-set-partuuid/00-run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -e
 
-IMGID="$(fdisk -l ${IMG_FILE} | sed -n 's/Disk identifier: 0x\([^ ]*\)/\1/p')"
+IMGID="$(fdisk -l "$IMG_FILE" | sed -n 's/Disk identifier: 0x\([^ ]*\)/\1/p')"
 
-BOOT_PARTUUID="${IMGID}-01"
-ROOT_PARTUUID="${IMGID}-02"
+BOOT_PARTUUID="$IMGID-01"
+ROOT_PARTUUID="$IMGID-02"
 
-sed -i "s/BOOTDEV/PARTUUID=${BOOT_PARTUUID}/" ${ROOTFS_DIR}/etc/fstab
-sed -i "s/ROOTDEV/PARTUUID=${ROOT_PARTUUID}/" ${ROOTFS_DIR}/etc/fstab
-
-sed -i "s/ROOTDEV/PARTUUID=${ROOT_PARTUUID}/" ${BOOTFS_DIR}/cmdline.txt
+sed -i "s/BOOTDEV/PARTUUID=$BOOT_PARTUUID/" "$ROOTFS_DIR/etc/fstab"
+sed -i "s/ROOTDEV/PARTUUID=$ROOT_PARTUUID/" "$ROOTFS_DIR/etc/fstab"
+sed -i "s/ROOTDEV/PARTUUID=$ROOT_PARTUUID/" "$BOOTFS_DIR/cmdline.txt"
