f72926758a3d4233071654bbc6fa579a69231646
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-02 23:01:46 +0000

    Move tasks to scripts

--- stage0/00-bootstrap/04-run.sh
+++ stage0/00-bootstrap/04-run.sh
@@ -18,3 +18,11 @@ libraspberrypi-bin \
 libraspberrypi0    \
 raspi-config
 EOF
+
+touch ${ROOTFS_DIR}/spindle_install
+
+on_chroot << EOF
+apt-get install -y raspi-copies-and-fills
+EOF
+
+rm -f ${ROOTFS_DIR}/spindle_install
--- stage0/00-bootstrap/05-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-touch ${ROOTFS_DIR}/spindle_install
-
-on_chroot << EOF
-apt-get install -y raspi-copies-and-fills
-EOF
-
-rm -f ${ROOTFS_DIR}/spindle_install
