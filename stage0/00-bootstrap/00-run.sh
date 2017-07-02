6041e4c5ba6cab464ef0869c122c531122d0317a
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-02 19:41:12 +0000

    Fix code style

--- stage0/00-bootstrap/00-run.sh
+++ stage0/00-bootstrap/00-run.sh
@@ -9,11 +9,11 @@ if [ ! -d "$ROOTFS_DIR" ]; then
     BOOTSTRAP_CMD='debootstrap'
   fi
 
-  capsh --drop=cap_setfcap -- -c "$BOOTSTRAP_CMD \
-    --components=main,contrib,non-free           \
-    --arch armhf                                 \
+  capsh --drop=cap_setfcap -- -c "$BOOTSTRAP_CMD   \
+    --components=main,contrib,non-free             \
+    --arch armhf                                   \
     --keyring $SUB_STAGE_DIR/files/raspberrypi.gpg \
-    jessie                                       \
-    $ROOTFS_DIR                                  \
+    jessie                                         \
+    $ROOTFS_DIR                                    \
     http://mirrordirector.raspbian.org/raspbian/" || rmdir "$ROOTFS_DIR/debootstrap"
 fi
