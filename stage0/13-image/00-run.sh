8a37703ab4c7588b850d80ae6c4c531f1b6595b2
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-02 18:24:32 +0000

    Fix code style

--- stage0/13-image/00-run.sh
+++ stage0/13-image/00-run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -e
 
-unmount_image ${IMG_FILE}
+unmount_image "$IMG_FILE"
 
-rm -f ${IMG_FILE}
+rm -f "$IMG_FILE"
 
-rm -rf ${ROOTFS_DIR}
-mkdir -p ${ROOTFS_DIR}
+rm -rf "$ROOTFS_DIR"
+mkdir -p "$ROOTFS_DIR"
 
-BOOT_SIZE=$(du --apparent-size -s ${ROOTFS_DIR}/boot --block-size=1 | cut -f 1)
-TOTAL_SIZE=$(du --apparent-size -s ${ROOTFS_DIR} --exclude var/cache/apt/archives --block-size=1 | cut -f 1)
+BOOT_SIZE=$(du --apparent-size -s "$ROOTFS_DIR/boot" --block-size=1 | cut -f 1)
+TOTAL_SIZE=$(du --apparent-size -s "$ROOTFS_DIR" --exclude var/cache/apt/archives --block-size=1 | cut -f 1)
 
 IMG_SIZE=$((BOOT_SIZE + TOTAL_SIZE + (800 * 1024 * 1024)))
 
-truncate -s ${IMG_SIZE} ${IMG_FILE}
-fdisk -H 255 -S 63 ${IMG_FILE} <<EOF
+truncate -s $IMG_SIZE "$IMG_FILE"
+fdisk -H 255 -S 63 "$IMG_FILE" <<EOF
 o
 n
 
@@ -33,7 +33,7 @@ p
 w
 EOF
 
-PARTED_OUT=$(parted -s ${IMG_FILE} unit b print)
+PARTED_OUT=$(parted -s "$IMG_FILE" unit b print)
 BOOT_OFFSET=$(echo "$PARTED_OUT" | grep -e '^ 1'| xargs echo -n \
 | cut -d" " -f 2 | tr -d B)
 BOOT_LENGTH=$(echo "$PARTED_OUT" | grep -e '^ 1'| xargs echo -n \
@@ -44,14 +44,14 @@ ROOT_OFFSET=$(echo "$PARTED_OUT" | grep -e '^ 2'| xargs echo -n \
 ROOT_LENGTH=$(echo "$PARTED_OUT" | grep -e '^ 2'| xargs echo -n \
 | cut -d" " -f 4 | tr -d B)
 
-BOOT_DEV=$(losetup --show -f -o ${BOOT_OFFSET} --sizelimit ${BOOT_LENGTH} ${IMG_FILE})
-ROOT_DEV=$(losetup --show -f -o ${ROOT_OFFSET} --sizelimit ${ROOT_LENGTH} ${IMG_FILE})
+BOOT_DEV=$(losetup --show -f -o $BOOT_OFFSET --sizelimit $BOOT_LENGTH "$IMG_FILE")
+ROOT_DEV=$(losetup --show -f -o $ROOT_OFFSET --sizelimit $ROOT_LENGTH "$IMG_FILE")
 echo "/boot: offset $BOOT_OFFSET, length $BOOT_LENGTH"
 echo "/:     offset $ROOT_OFFSET, length $ROOT_LENGTH"
 
 mkdosfs -n boot -F 32 -v $BOOT_DEV > /dev/null
 mkfs.ext4 -O ^huge_file $ROOT_DEV > /dev/null
 
-mount -v $ROOT_DEV ${ROOTFS_DIR} -t ext4
-mkdir -p ${ROOTFS_DIR}/boot
-mount -v $BOOT_DEV ${ROOTFS_DIR}/boot -t vfat
+mount -v $ROOT_DEV "$ROOTFS_DIR" -t ext4
+mkdir -p "$ROOTFS_DIR/boot"
+mount -v $BOOT_DEV "$ROOTFS_DIR/boot" -t vfat
