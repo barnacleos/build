6a4267e9f6dde8224993736758095e98aaa82469
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-02 23:12:08 +0000

    Alignn attributes

--- stage0/00-bootstrap/12-run.sh
+++ stage0/00-bootstrap/12-run.sh
@@ -49,11 +49,11 @@ raspberrypi-net-mods \
 dhcpcd5
 EOF
 
-install -v -d ${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d
+install -v -d                     ${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d
 install -v -m 644 files/wait.conf ${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/
 
-install -v -d                                           ${ROOTFS_DIR}/etc/wpa_supplicant
-install -v -m 600 files/wpa_supplicant.conf             ${ROOTFS_DIR}/etc/wpa_supplicant/
+install -v -d                               ${ROOTFS_DIR}/etc/wpa_supplicant
+install -v -m 600 files/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_supplicant/
 
 on_chroot << EOF
 apt-get install -y vim
