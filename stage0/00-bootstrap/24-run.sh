0c5c055e63cebc7ae2192d147217965205deb1f5
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-02 22:58:17 +0000

    Mve tasks to script

--- stage0/00-bootstrap/24-run.sh
+++ stage0/00-bootstrap/24-run.sh
@@ -1,6 +1,23 @@
 #!/bin/bash -e
 
 on_chroot << EOF
+apt-get update
+apt-get -y dist-upgrade
+apt-get clean
+EOF
+
+install -m 644 files/resolv.conf ${ROOTFS_DIR}/etc/
+
+IMGID="$(fdisk -l "$IMG_FILE" | sed -n 's/Disk identifier: 0x\([^ ]*\)/\1/p')"
+
+BOOT_PARTUUID="$IMGID-01"
+ROOT_PARTUUID="$IMGID-02"
+
+sed -i "s/BOOTDEV/PARTUUID=$BOOT_PARTUUID/" "$ROOTFS_DIR/etc/fstab"
+sed -i "s/ROOTDEV/PARTUUID=$ROOT_PARTUUID/" "$ROOTFS_DIR/etc/fstab"
+sed -i "s/ROOTDEV/PARTUUID=$ROOT_PARTUUID/" "$BOOTFS_DIR/cmdline.txt"
+
+on_chroot << EOF
 /etc/init.d/fake-hwclock stop
 hardlink -t /usr/share/doc
 EOF
