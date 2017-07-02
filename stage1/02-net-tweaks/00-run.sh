f6e269a77d89302d9ed4d0b8e80ba7cc96d68c98
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-02 15:08:24 +0000

    Add host "barnacleos"

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -6,6 +6,8 @@ install -m 644 files/interfaces ${ROOTFS_DIR}/etc/network/interfaces
 echo $HOSTNAME >${ROOTFS_DIR}/etc/hostname
 chmod 644       ${ROOTFS_DIR}/etc/hostname
 
+echo "127.0.1.1 $HOSTNAME" >>/etc/hosts
+
 on_chroot << EOF
 dpkg-divert --add --local /lib/udev/rules.d/75-persistent-net-generator.rules
 EOF
