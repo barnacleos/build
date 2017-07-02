7c77e1a7a6eaf343bc7ff70019125268dbf0328a
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-02 12:05:50 +0000

    Fix code style

--- functions/dependencies_check.sh
+++ functions/dependencies_check.sh
@@ -5,25 +5,25 @@
 # a : and the name of a package if the package on a Debian-ish system is not
 # named for the tool (i.e., qemu-user-static).
 dependencies_check() {
-	local depfile deps missing
+  local depfile deps missing
 
-	for depfile in "$@"; do
-		if [[ -e "$depfile" ]]; then
-			deps="$(sed -f "${SCRIPT_DIR}/remove-comments.sed" < ${BASE_DIR}/depends)"
+  for depfile in "$@"; do
+    if [[ -e "$depfile" ]]; then
+      deps="$(sed -f "${SCRIPT_DIR}/remove-comments.sed" < ${BASE_DIR}/depends)"
+    fi
 
-		fi
-		for dep in $deps; do
-			if ! hash ${dep%:*} 2>/dev/null; then
-				missing="${missing:+$missing }${dep#*:}"
-			fi
-		done
-	done
+    for dep in $deps; do
+      if ! hash ${dep%:*} 2>/dev/null; then
+        missing="${missing:+$missing }${dep#*:}"
+      fi
+    done
+  done
 
-	if [[ "$missing" ]]; then
-		echo "Reqired dependencies not installed"
-		echo
-		echo "This can be resolved on Debian/Raspbian systems by installing:"
-		echo "$missing"
-		false
-	fi
+  if [[ "$missing" ]]; then
+    echo "Reqired dependencies not installed"
+    echo
+    echo "This can be resolved on Debian/Raspbian systems by installing:"
+    echo "$missing"
+    false
+  fi
 }
