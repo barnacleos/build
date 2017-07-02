4c842115df694272cc2881f37447b6a6e90b0c8b
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-02 23:11:31 +0000

    Remove tabulation

--- stage0/00-bootstrap/12-run.sh
+++ stage0/00-bootstrap/12-run.sh
@@ -49,8 +49,8 @@ raspberrypi-net-mods \
 dhcpcd5
 EOF
 
-install -v -d						${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d
-install -v -m 644 files/wait.conf			${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/
+install -v -d ${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d
+install -v -m 644 files/wait.conf ${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/
 
 install -v -d                                           ${ROOTFS_DIR}/etc/wpa_supplicant
 install -v -m 600 files/wpa_supplicant.conf             ${ROOTFS_DIR}/etc/wpa_supplicant/
