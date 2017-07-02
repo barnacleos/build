5be75eb3cbdb7d333d9d62afc09e4e4dd703a784
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-02 23:43:44 +0000

    Move tasks to script

--- stage0/00-substage/04-run.sh
+++ stage0/00-substage/04-run.sh
@@ -46,3 +46,48 @@ keyboard-configuration keyboard-configuration/variant       select English (UK)
 
 SELEOF
 EOF
+
+on_chroot << EOF
+apt-get install -y     \
+ssh                    \
+less                   \
+fbset                  \
+sudo                   \
+psmisc                 \
+strace                 \
+module-init-tools      \
+ed                     \
+ncdu                   \
+crda                   \
+console-setup          \
+keyboard-configuration \
+debconf-utils          \
+parted                 \
+unzip                  \
+manpages-dev           \
+bash-completion        \
+gdb                    \
+pkg-config             \
+v4l-utils              \
+avahi-daemon           \
+hardlink               \
+ca-certificates        \
+curl                   \
+fake-hwclock           \
+ntp                    \
+usbutils               \
+libraspberrypi-dev     \
+libraspberrypi-doc     \
+libfreetype6-dev       \
+dosfstools             \
+dphys-swapfile         \
+raspberrypi-sys-mods   \
+apt-listchanges        \
+usb-modeswitch         \
+apt-transport-https    \
+libpam-chksshpwd
+EOF
+
+on_chroot << EOF
+apt-get install --no-install-recommends -y cifs-utils
+EOF
