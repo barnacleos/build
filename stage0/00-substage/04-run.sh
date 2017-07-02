7b81178753bc90682f2ebcacc2383f2e8e86faa5
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-02 23:52:26 +0000

    Move tasks to script

--- stage0/00-substage/04-run.sh
+++ stage0/00-substage/04-run.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -e
 
+install -d                        "$ROOTFS_DIR/etc/systemd/system/getty@tty1.service.d"
+install -m 644 files/noclear.conf "$ROOTFS_DIR/etc/systemd/system/getty@tty1.service.d/noclear.conf"
+install -m 744 files/policy-rc.d  "$ROOTFS_DIR/usr/sbin/policy-rc.d" #TODO: Necessary in systemd?
+install -m 644 files/fstab        "$ROOTFS_DIR/etc/fstab"
+
+on_chroot << EOF
+if ! id -u $USERNAME >/dev/null 2>&1; then
+	adduser --disabled-password --gecos "" $USERNAME
+fi
+echo "$USERNAME:$PASSWORD" | chpasswd
+passwd -d root
+EOF
+
 install -m 644 files/ipv6.conf  "$ROOTFS_DIR/etc/modprobe.d/ipv6.conf"
 install -m 644 files/interfaces "$ROOTFS_DIR/etc/network/interfaces"
 
