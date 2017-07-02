c2cd36326742a51642b295775b2030d469fa3b5b
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-02 12:44:47 +0000

    Fix indentation

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -e
 
 bootstrap() {
-	local ARCH
-	ARCH=$(dpkg --print-architecture)
+  local ARCH
+  ARCH=$(dpkg --print-architecture)
 
-	if [ "$ARCH" !=  "armhf" ]; then
-		local BOOTSTRAP_CMD=qemu-debootstrap
-	else
-		local BOOTSTRAP_CMD=debootstrap
-	fi
+  if [ "$ARCH" !=  "armhf" ]; then
+    local BOOTSTRAP_CMD=qemu-debootstrap
+  else
+    local BOOTSTRAP_CMD=debootstrap
+  fi
 
-	capsh --drop=cap_setfcap -- -c "${BOOTSTRAP_CMD} --components=main,contrib,non-free \
-		--arch armhf \
-		--keyring "${STAGE_DIR}/files/raspberrypi.gpg" \
-		$1 $2 $3" || rmdir "$2/debootstrap"
+  capsh --drop=cap_setfcap -- -c "${BOOTSTRAP_CMD} --components=main,contrib,non-free \
+    --arch armhf \
+    --keyring "${STAGE_DIR}/files/raspberrypi.gpg" \
+    $1 $2 $3" || rmdir "$2/debootstrap"
 }
 
 if [ ! -d ${ROOTFS_DIR} ]; then
-	bootstrap jessie ${ROOTFS_DIR} http://mirrordirector.raspbian.org/raspbian/
+  bootstrap jessie ${ROOTFS_DIR} http://mirrordirector.raspbian.org/raspbian/
 fi
