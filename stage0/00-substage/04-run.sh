ce1d7a7584a38a7ebcfba534b9db421af144d45e
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-03 01:16:00 +0000

    Reorder code

--- stage0/00-substage/04-run.sh
+++ stage0/00-substage/04-run.sh
@@ -4,6 +4,13 @@ install -d                        "$ROOTFS_DIR/etc/systemd/system/getty@tty1.ser
 install -m 644 files/noclear.conf "$ROOTFS_DIR/etc/systemd/system/getty@tty1.service.d/noclear.conf"
 install -m 744 files/policy-rc.d  "$ROOTFS_DIR/usr/sbin/policy-rc.d" #TODO: Necessary in systemd?
 install -m 644 files/fstab        "$ROOTFS_DIR/etc/fstab"
+install -m 644 files/ipv6.conf    "$ROOTFS_DIR/etc/modprobe.d/ipv6.conf"
+install -m 644 files/interfaces   "$ROOTFS_DIR/etc/network/interfaces"
+
+echo $HOSTNAME > "$ROOTFS_DIR/etc/hostname"
+chmod 644        "$ROOTFS_DIR/etc/hostname"
+
+echo "127.0.1.1 $HOSTNAME" >>/etc/hosts
 
 on_chroot << EOF
 if ! id -u $USERNAME >/dev/null 2>&1; then
@@ -12,14 +19,6 @@ fi
 echo "$USERNAME:$PASSWORD" | chpasswd
 passwd -d root
 EOF
-
-install -m 644 files/ipv6.conf  "$ROOTFS_DIR/etc/modprobe.d/ipv6.conf"
-install -m 644 files/interfaces "$ROOTFS_DIR/etc/network/interfaces"
-
-echo $HOSTNAME > "$ROOTFS_DIR/etc/hostname"
-chmod 644        "$ROOTFS_DIR/etc/hostname"
-
-echo "127.0.1.1 $HOSTNAME" >>/etc/hosts
 
 on_chroot << EOF
 dpkg-divert --add --local /lib/udev/rules.d/75-persistent-net-generator.rules
