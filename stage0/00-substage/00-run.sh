781bfbe8a5dc6872b6b884bf1a3b5b5630df8682
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-03 04:20:37 +0000

    Add empty line

--- stage0/00-substage/00-run.sh
+++ stage0/00-substage/00-run.sh
@@ -22,6 +22,7 @@ install -m 644 files/sources.list "$ROOTFS_DIR/etc/apt/"
 install -m 644 files/raspi.list   "$ROOTFS_DIR/etc/apt/sources.list.d/"
 
 on_chroot apt-key add - < files/raspberrypi.gpg.key
+
 on_chroot << EOF
 apt-get update
 apt-get dist-upgrade -y
