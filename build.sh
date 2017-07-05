ed7d3d112f99b677421bf768825fdaf8e1984d64
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-05 08:58:12 +0000

    Remove network persistence patch

--- build.sh
+++ build.sh
@@ -226,12 +226,6 @@ EOF
 ##
 # Configure network.
 #
-apply_patches '02-persistant-net.diff'
-
-on_chroot << EOF
-dpkg-divert --add --local /lib/udev/rules.d/75-persistent-net-generator.rules
-EOF
-
 install -m 644 files/resolv.conf "$ROOTFS_DIR/etc/"
 install -m 644 files/interfaces  "$ROOTFS_DIR/etc/network/interfaces"
 install -m 644 files/ipv6.conf   "$ROOTFS_DIR/etc/modprobe.d/ipv6.conf"
