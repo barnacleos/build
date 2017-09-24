bd22a08a36badba58375de6146863f4f2ca52556
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-09-24 18:16:56 +0000

    Fix spec

--- test/partition_alignment.sh
+++ test/partition_alignment.sh
@@ -1,9 +1,9 @@
 #!/bin/false
 
-TABLE="$(fdisk -o Device,Start -l "$IMG_FILE" | grep "^$IMG_FILE")"
+TABLE="$(fdisk -l "$IMG_FILE" | grep "^$IMG_FILE")"
 
 test 2 -eq $(echo "$TABLE" | wc -l)
 
-STARTS="$(echo "$TABLE" | sed "s|^$IMG_FILE. ||")"
+TABLE="$(echo "$TABLE" | sed "s|^$IMG_FILE. ||")"
 
-echo "$STARTS" | while read -r offset; do test 0 -eq $((offset % 4096)); done
+echo "$TABLE" | while read -r line; do test 0 -eq $(("$(echo $line | cut -d ' ' -f 1)" % 4096)); done
