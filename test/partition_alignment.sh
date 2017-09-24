64b7f93c14d3f90a15af7bd7adf2fca751b9a3a9
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-09-24 18:45:08 +0000

    Try to fix test syntax

--- test/partition_alignment.sh
+++ test/partition_alignment.sh
@@ -6,4 +6,4 @@ test 2 -eq $(echo "$TABLE" | wc -l)
 
 TABLE="$(echo "$TABLE" | sed "s|^$IMG_FILE. ||")"
 
-echo "$TABLE" | while read -r line; do test 0 -eq $(("$(echo $line | cut -d ' ' -f 1)" % 4096)); done
+echo "$TABLE" | while read -r line; do test 0 -eq $(($(echo $line | cut -d ' ' -f 1) % 4096)); done
