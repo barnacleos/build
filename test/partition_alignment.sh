9e9bec0e472be74bc7c8dc7d0066dd0e7248cd76
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-09-24 19:42:20 +0000

    fuck you

--- test/partition_alignment.sh
+++ test/partition_alignment.sh
@@ -6,4 +6,17 @@ test 2 -eq $(echo "$TABLE" | wc -l)
 
 TABLE="$(echo "$TABLE" | sed "s|^$IMG_FILE. ||")"
 
-echo "$TABLE" | while read -r line; do test 0 -eq $(($(echo $line | cut -d ' ' -f 1) % 4096)); done
+echo '--- table ---'
+echo "$TABLE" | while read -r line; do echo $line; done
+
+STARTS=$(echo "$TABLE" | while read -r line; do echo "$(echo $line | cut -d ' ' -f 1)"; done)
+
+echo '--- starts ---'
+echo "$STARTS" | while read -r line; do echo $line; done
+
+REMS=$(echo "$STARTS" | while read -r line; do echo $(($line % 4096)); done)
+
+echo '--- rems ---'
+echo "$REMS" | while read -r line; do echo $line; done
+
+echo "$REMS" | while read -r line; do test 0 -eq $line; done
