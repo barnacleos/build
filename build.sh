20393d307291c0f64886627a444d514cbbde33ee
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-09-24 15:13:15 +0000

    Remove unnecessary condition

--- build.sh
+++ build.sh
@@ -156,23 +156,21 @@ if [ ! -d "$ROOTFS_DIR" ]; then
 ##
 # Bootstrap a basic Debian system.
 #
-if [ ! -d "$ROOTFS_DIR" ]; then
-  ARCH="$(dpkg --print-architecture)"
+ARCH="$(dpkg --print-architecture)"
 
-  if [ "$ARCH" != 'armhf' ]; then
-    BOOTSTRAP_CMD='qemu-debootstrap'
-  else
-    BOOTSTRAP_CMD='debootstrap'
-  fi
-
-  capsh --drop=cap_setfcap -- -c "$BOOTSTRAP_CMD     \
-    --components=main,contrib,non-free               \
-    --arch armhf                                     \
-    --keyring $KEYS_DIR/raspbian-archive-keyring.gpg \
-    jessie                                           \
-    $ROOTFS_DIR                                      \
-    http://mirrordirector.raspbian.org/raspbian/" || rmdir "$ROOTFS_DIR/debootstrap/"
+if [ "$ARCH" != 'armhf' ]; then
+  BOOTSTRAP_CMD='qemu-debootstrap'
+else
+  BOOTSTRAP_CMD='debootstrap'
 fi
+
+capsh --drop=cap_setfcap -- -c "$BOOTSTRAP_CMD     \
+  --components=main,contrib,non-free               \
+  --arch armhf                                     \
+  --keyring $KEYS_DIR/raspbian-archive-keyring.gpg \
+  jessie                                           \
+  $ROOTFS_DIR                                      \
+  http://mirrordirector.raspbian.org/raspbian/" || rmdir "$ROOTFS_DIR/debootstrap/"
 
 ##
 # Prepare for Quilt patching.
